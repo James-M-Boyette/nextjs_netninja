b67fa6da8c153c63927adddf08efaa2806e08315
sou7hernsaint <James.M.84.Boyette@gmail.com>
2022-06-02 16:12:49 -0400

    Create pages & components-templates

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div>
+            <h4>Copyright Boyette Productions, 2022</h4>
+        </div>
+     );
+}
+ 
+export default Footer;
---
+++ pages/users/index.js
@@ -0,0 +1,18 @@
+import Navbar from "../../comps/Navbar"
+import Footer from "../../comps/Footer"
+
+
+const Ninjas = () => {
+    return ( 
+        <div>
+
+            <h1>All Ninjas</h1>
+      
+            
+
+        </div>
+        
+     );
+}
+ 
+export default Ninjas;
